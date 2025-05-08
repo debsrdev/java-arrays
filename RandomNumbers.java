1b4c7298abc66af664df6105cc4cc8a58ff51822
Débora <debora.rubio.soliva@gmail.com>
2025-05-08 17:24:20 +0200

    add random numbers exercise

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -1,7 +1,12 @@
 public class RandomNumbers {
     public static void main(String[] args) {
         
-        //Escribe un programa que genere un array de 2 dimensiones de 100 filas y 10 columnas  con números aleatorios del 0 al 99, a través de un bucle anidado (Si no te suenan estos conceptos, ¡búscalos!)
+        //Escribe un programa que genere un array de 2 dimensiones de 100 filas y 10 columnas
+        // con números aleatorios del 0 al 99, a través de un bucle anidado
+        // (Si no te suenan estos conceptos, ¡búscalos!)
+        System.out.println("Matriz de 100 x 10 con números aleatorios del 0 al 99: \n");
+        int[][] array2D = new int[100][10];
+        print2DArray(array2D);
 
     }
 
@@ -13,7 +18,9 @@ public class RandomNumbers {
      * Inside the function:
      * 1. generate random numbers from 0 to 99
      */
-
+    public static int randomNumber() {
+        return (int)(Math.random()*100);
+    }
 
     /**
      * Function name: print2DArray
@@ -23,5 +30,13 @@ public class RandomNumbers {
      * Inside the function:
      * 1. nested loop that prints a 2D array using the randomNumber function
      */
-
+    public static void print2DArray(int[][] array2D) {
+        for (int i = 0; i < array2D.length; i++) {
+            for (int j = 0; j < array2D[i].length; j++) {
+                array2D[i][j] = randomNumber();
+                System.out.print(array2D[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
